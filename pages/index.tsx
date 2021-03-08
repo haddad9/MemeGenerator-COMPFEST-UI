4dcee52994fa90d8f2604ec3922a0da6c335c04d
Haddad <emha.haddad@gmail.com>
2021-03-08 09:44:59 +0700

    change tittle

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>c0mPfsstZ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
